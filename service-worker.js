874f4e0a1941bc0d4bcd11bd17ec56bb3a29ac61
Alex Barnett <alexbarnett@mac.mynetworksettings.com>
2026-05-03 23:55:05 -0400

    Bump service worker to v9

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Caches books you're reading for offline access
 
-// Book Notes Service Worker v8
-var CACHE = 'books-v8';
+// Book Notes Service Worker v9
+var CACHE = 'books-v9';
 
 // Core assets always cached
 var CORE = [
